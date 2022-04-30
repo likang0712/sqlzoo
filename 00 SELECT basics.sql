068a96d710b2c5d3debccba16ad211900c5550d0
kang <likang0712@gmail.com>
2022-05-01 11:30:14 +1200

    Update 00 SELECT basics.sql

--- 00 SELECT basics.sql
+++ 00 SELECT basics.sql
@@ -8,7 +8,6 @@ FROM world
 WHERE name = 'Germany'
 
 -- 2. Checking a list The word IN allows us to check if an item is in a list. The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.
-
 -- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 SELECT name, population 
 FROM world
