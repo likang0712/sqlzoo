5acbbe8bce221c72d13609a584bb0f01f143fa93
kang <likang0712@gmail.com>
2022-05-01 11:20:00 +1200

    Update aa.sql

--- aa.sql
+++ aa.sql
@@ -1,1 +1,14 @@
-a
+-- 1. 
+SELECT * 
+FROM world
+WHERE name = 'Germany'
+
+-- 2. 
+SELECT name, population 
+FROM world
+WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+-- 3. 
+SELECT name, area 
+FROM world
+WHERE area BETWEEN 200000 AND 250000
