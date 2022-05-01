a6adf9c4157eb4087bb5f740c8f9bf5c6b2cc5dc
kang <likang0712@gmail.com>
2022-05-01 12:04:26 +1200

    Update 03 SELECT from Nobel.sql

--- 03 SELECT from Nobel.sql
+++ 03 SELECT from Nobel.sql
@@ -94,3 +94,24 @@ FROM nobel
 WHERE yr=1984
 ORDER BY subject IN ('Physics','Chemistry'), subject, winner
 
+--------------------------------------------------------
+--------------------------------------------------------
+-- quiz for above (part of)
+-- 3. Pick the code that shows the amount of years where no Medicine awards were given
+SELECT COUNT(DISTINCT yr) FROM nobel
+ WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine')
+
+-- 5. Select the code which would show the year when neither a Physics or Chemistry award was given
+SELECT yr FROM nobel
+ WHERE yr NOT IN(SELECT yr 
+                   FROM nobel
+                 WHERE subject IN ('Chemistry','Physics'))
+ 
+-- 6. Select the code which shows the years when a Medicine award was given but no Peace or Literature award was
+SELECT DISTINCT yr
+  FROM nobel
+ WHERE subject='Medicine' 
+   AND yr NOT IN(SELECT yr FROM nobel 
+                  WHERE subject='Literature')
+   AND yr NOT IN (SELECT yr FROM nobel
+                   WHERE subject='Peace')
